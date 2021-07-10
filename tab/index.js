63ecadbc0484646935fde88dd1a8212587f361dd
YusukeNaito08 <yusuke0208na@gmail.com>
2021-07-10 23:34:40 +0900

    firstCommit

---
+++ tab/index.js
@@ -0,0 +1,16 @@
+'user strict'
+
+document.addEventListener('DOMContentLoaded' function () {
+
+  const tabs = document.getElementsByClassName('tab')
+
+
+}
+
+function tabSwitch() {
+  document.getElementsByClassName('is-active')[0].classList.remove('is-active');
+  this.classList.add('is-active');
+
+  document.getElementsByClassName('is-show')[0].classList.remove('is-show');
+
+});
