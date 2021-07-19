8c5b47b58f83ea941e011763e39e54a530fe6dd4
YusukeNaito08 <yusuke0208na@gmail.com>
2021-07-19 10:03:09 +0900

    NEED:FIX

--- sliderImageJS/index.js
+++ sliderImageJS/index.js
@@ -1,34 +1,37 @@
-const slide = document.getElementById("slider");
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn3");
-const btn4 = document.getElementById("btn4");
+const slides = document.querySelectorAll("slide");
+// const btn1 = document.getElementById("btn1");
+// const btn2 = document.getElementById("btn2");
+// const btn3 = document.getElementById("btn3");
+// const btn4 = document.getElementById("btn4");
 
-btn1.onclick = function () {
-  slide.style.transform = "translateX(0px)";
-  btn1.classList.add("active");
-  btn2.classList.remove("active");
-  btn3.classList.remove("active");
-  btn4.classList.remove("active");
+// btn1.onclick = function () {
+//   slide.style.transform = "translateX(0px)";
+// };
+// btn2.onclick = function () {
+//   slide.style.transform = "translateX(-100%)";
+
+//   btn3.onclick = function () {
+//     slide.style.transform = "translateX(-200%)";
+//   };
+//   btn4.onclick = function () {
+//     slide.style.transform = "translateX(-300%)";
+//   };
+
+var btns = document.querySelectorAll("#btn");
+
+var navs = function (nav) {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+  });
+  btns[nav].classList.add("active");
+  slides[nav].classList.add("active");
 };
-btn2.onclick = function () {
-  slide.style.transform = "translateX(-100%)";
-  btn1.classList.remove("active");
-  btn2.classList.add("active");
-  btn3.classList.remove("active");
-  btn4.classList.remove("active");
-};
-btn3.onclick = function () {
-  slide.style.transform = "translateX(-200%)";
-  btn1.classList.remove("active");
-  btn2.classList.remove("active");
-  btn3.classList.add("active");
-  btn4.classList.remove("active");
-};
-btn4.onclick = function () {
-  slide.style.transform = "translateX(-300%)";
-  btn1.classList.remove("active");
-  btn2.classList.remove("active");
-  btn3.classList.remove("active");
-  btn4.classList.add("active");
-};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    navs(i);
+  });
+});
